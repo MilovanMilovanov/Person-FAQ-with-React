46bbc2dce90500b0d3e1c902032ce7a1892864c7
MilovanMilovanov <ninapredninazad@gmail.com>
2021-07-13 21:03:48 +0300

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import getRandomColor from './utils';
 
 function Header({ titleColor }) {
-    const title = <span className={titleColor ? 'react' : ''}>React</span>;
+    const title = <span style={{ color: titleColor ? getRandomColor(): '' }}> React</span >;
     return <h1>My first {title} project</h1>
 }
 
