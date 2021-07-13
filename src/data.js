1b22a717c2892255e6e1cf39ec1d2554f21a4aa1
MilovanMilovanov <ninapredninazad@gmail.com>
2021-07-13 21:01:55 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,11 +1,8 @@
-const QandA =  [
-    { title: 'When did you learn React?', answer: 'I watched 4 hour introduction course on the day i received the email with the task, to prepare myself :)', open: false },
-    { title: 'Why did you choose JavaScript?', answer: 'It gives me the freedom to mix my creative background with engineering, which i\'m very keen to learn.', open: false },
+export default [
+    { title: 'How did you learn React?', answer: 'I started by reading the documentation and mixed it with online tutorials later on after i got the hang of some basic operations.', open: false },
+    { title: 'Why did you choose JavaScript?', answer: 'It gives me the freedom to mix my creative background with engineering, which i\'m very keen on learning.', open: false },
     { title: 'Do you like challenges?', answer: 'I certainly do and this is a small sample :)', open: false },
     { title: 'What is your motivation?', answer: 'I take pride in everything i do, as i\'m result driven person, that motivates me to get things done!', open: false },
-    { title: 'What do you expect from us?', answer: 'Possibilities, that will put me in a position to grow and nothing less!', open: false },
+    { title: 'What do you expect from us?', answer: 'Possibilities, that will put me in a position of growth in my skillset and contribution, nothing less!', open: false },
     { title: 'Any final message for us?', answer: 'I enjoyed the process of creating this task, and i\'m looking forward to the next step :)!', open: false }
 ];
-
-export default QandA;
-
